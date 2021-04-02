6dc4f097f5b70424608a49a264735c4f700bb453
gingkolane <gingkolane@gmail.com>
2021-04-02 01:34:13 -0400

    change to Better-sqlite API library

--- checkDBconnection.js
+++ checkDBconnection.js
@@ -4,7 +4,15 @@ const db = new Database('./persondb.db', { verbose: console.log });
 // const row = db.prepare('SELECT * FROM users WHERE id = ?').get(userId);
 // console.log(row.firstName, row.lastName, row.email);
 
-const stmt = db.prepare('SELECT * FROM PERSON WHERE PRSN_LAST_NAME = ?');
-const foundPerson = stmt.get('Lennon');
+// const stmt = db.prepare('SELECT * FROM PERSON WHERE PRSN_LAST_NAME = ?');
+// const foundPerson = stmt.get('Lennon');
 
-console.log(foundPerson.PRSN_ID); 
+// console.log(foundPerson.PRSN_ID); 
+
+const stmt = db.prepare(`select * from PERSON WHERE PRSN_ID = 1`)
+        
+const foundPerson = stmt.get();
+console.log(foundPerson)
+
+
+// const row = db.prepare('SELECT * FROM users WHERE id = ?').get(userId);
