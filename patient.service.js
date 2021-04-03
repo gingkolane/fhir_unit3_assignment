8a0b1ffcaef87d2b313a2726ebfaa99110c397f4
gingkolane <gingkolane@gmail.com>
2021-04-03 00:42:26 -0400

    create working

--- patient.service.js
+++ patient.service.js
@@ -1,11 +1,11 @@
 const { loggers, resolveSchema } = require('@asymmetrik/node-fhir-server-core');
 const logger = loggers.get('default');
 const { v4: uuidv4 } = require('uuid');
+const Database = require('better-sqlite3');
 const Patient = resolveSchema('4_0_0', 'patient'); // const Patient = resolveSchema(args.base_version, 'patient'); args not available yet, so put in version directly
 const BundleEntry = resolveSchema('4_0_0', 'bundleentry');
 const Bundle = resolveSchema('4_0_0', 'bundle');
-const OperationOutcome = require('@asymmetrik/node-fhir-server-core/src/server/resources/4_0_0/schemas/operationoutcome');
-const Database = require('better-sqlite3');
+const OperationOutcome = resolveSchema('4_0_0','operationoutcome');
 
 //Meta data for FHIR R4
 let getMeta = (base_version) => {
@@ -28,7 +28,7 @@ function mapPersonToPatientResource(person) {
     let patient = new Patient
 
     // set data from person to patient resource instance
-    patient.id = person.PRSN_ID.toString();
+    patient.id = person.PRSN_ID;
     patient.gender = person.PRSN_GENDER;
     patient.birthDate = person.PRSN_BIRTH_DATE;
     patient.telecom = [{
@@ -50,7 +50,7 @@ function mapPersonToPatientResource(person) {
         }
     ]
 
-    patient.identifier = mapPersonDocToPatientIdentifier(person)
+    patient.identifier = mapPersonDocRecordToPatientIdentifier(person)
 
     patient.text = {
         "status": "generated",
@@ -61,8 +61,8 @@ function mapPersonToPatientResource(person) {
 }
 
 // convert person doc information into patient identifier
-function mapPersonDocToPatientIdentifier(person) {
-    //every patient has the hospital_id as the default identifier
+function mapPersonDocRecordToPatientIdentifier(person) {
+    //every patient has the hospital_id as the default identifier, patientIdentifier is an array
     let patientIdentifier = [{
         use: "official",
         system: "https://saintmartinhospital.org/patient-id",
@@ -70,7 +70,7 @@ function mapPersonDocToPatientIdentifier(person) {
         period: {start: person.PRSN_CREATE_DATE }
     }]
 
-    //open a database conneciton and get all personDoc records related to a person
+    // get all personDoc records related to a person
     const db = new Database('./persondb.db', { verbose: console.log })
     const stmt = db.prepare(`select * from PERSON_DOC where PERSON_DOC.PRDT_PRSN_ID = ?`)
     const personDocArray = stmt.all('person.PRSN_ID')
@@ -99,72 +99,62 @@ function mapPersonDocToPatientIdentifier(person) {
             default: 
                 break;
         } 
-        return patientIdentifier;
     })
+    return patientIdentifier;
+}
+
+function createPersonDocRecord(personId,identifier) {
+
+    let personDocObj={};
+
+    personDocObj.PRDT_PRSN_ID = personId,
+    // personDocObj.PRDT_DCTP_ID = (identifier.system === "https://www.national-office.gov/ni") ? 1:2
+
+    personDocObj.PRDT_DCTP_ID = 
+        (identifier.system === "https://www.national-office.gov/ni") ? 1
+        :(identifier.system === "https://www.foreign-affairs.gov/pp") ? 2
+        :null
+
+    personDocObj.PRDT_DOC_VALUE = identifier.value
+    personDocObj.PRDT_CREATE_DATE = new Date().toISOString(),
+    personDocObj.PRDT_DELETE_DATE = ''
+
+    const columnNames = "(PRDT_PRSN_ID, PRDT_DCTP_ID, PRDT_DOC_VALUE, PRDT_CREATE_DATE, PRDT_DELETE_DATE)"
+    
+    const db = new Database('./persondb.db', { verbose: console.log })
+    const stmt = db.prepare(`INSERT INTO PERSON_DOC ${columnNames} VALUES (?, ?, ?, ?, ?)`);
+    const info = stmt.run(personDocObj.PRDT_PRSN_ID, personDocObj.PRDT_DCTP_ID, personDocObj.PRDT_DOC_VALUE, personDocObj.PRDT_CREATE_DATE, personDocObj.PRDT_DELETE_DATE );
 
 }
 
-function mapPatientIdentifierToPatient(identifier) {
+async function searchByIdentifier(identifier) {
 
     let identifierArray = identifier.split("|"); // [ 'https://saintmartinhospital.org/patient-id', '1' ]
     let identifier_system = identifierArray[0]
-    let identifier_value = identifierArray[0]
+    let identifier_value = identifierArray[1]
 
     if (identifier_system === "https://saintmartinhospital.org/patient-id") {
         return searchById(identifier_value)
         }
     
+    let PRDT_DCTP_ID = 0
     if (identifier_system === "https://www.national-office.gov/ni") {PRDT_DCTP_ID = 1}
-        
     if (identifier_system === "https://www.foreign-affairs.gov/pp") {PRDT_DCTP_ID = 2}
 
-    // filter persons with the specific docType
-    let stmt_docType= db.prepare('select PRDT_PRSN_ID from PERSON_DOC WHERE PRDT_DCTP_ID like ?');
-    let personArray1 = stmt_docType.all(PRDT_DCTP_ID)
-
-    //find all persons of the docValue
-    const stmt_docValue = db.prepare('select PRDT_PRSN_ID from PERSON_DOC WHERE PRDT_DOC_VALUE like ?');
-    let personArray2 = stmt_docValue.all(identifier_value)
-
-    let personArray = [...personArray1, ...personArray2]
-    let uniqueArray = [...new Set(personArray)];
-    foundPerson = uniqueArray[0]
-    return searchById(foundPerson.id)
-}
-
-function createOnePersonDocRecord(personId,identifier) {
-
-    let personDocObj;
-
-    personDocObj.PRDT_PRSN_ID = personId,
-    personDocObj.PRDT_DOC_VALUE = identifier.value
-    personDocObj.PRDT_CREATE_DATE = new Date().toISOString(),
-    personDocObj.PRDT_DELETE_DATE = ''
-    if (identifier.system === "https://www.national-office.gov/ni") personDocObj.PRDT_DCTP_ID = 1
-    if (identifier.system === "https://www.foreign-affairs.gov/pp") personDocObj.PRDT_DCTP_ID = 2
-
-    const personDocValues = personDocObj.values.join(',')
-
-    const columnNames = (
-        // "PRDT_ID", 
-        "PRDT_PRSN_ID",
-        "PRDT_DCTP_ID",
-        "PRDT_DOC_VALUE",
-        "PRDT_CREATE_DATE",
-        "PRDT_DELETE_DATE"
-        )
-    
+    // create db connection
     const db = new Database('./persondb.db', { verbose: console.log })
+    // filter persons with the specific docType
+    let stmt = db.prepare('SELECT * from PERSON where PERSON.PRSN_ID = (SELECT PRDT_PRSN_ID from PERSON_DOC WHERE PRDT_DCTP_ID = ? AND PRDT_DOC_VALUE = ?)');
+    //get one person fitting the criteria
+    let personFound = stmt.get(PRDT_DCTP_ID, identifier_value)
 
-    const stmt = db.prepare(`INSERT INTO PERSON_DOC ${columnNames} VALUES (?, ?, ?, ?, ?)`);
-    const info = stmt.run(personDocValues);
+    let patient = await mapPersonToPatientResource(personFound)
 
-    // info.lastInsertRowid: the rowid of the last row inserted
-    return info.lastInsertRowId
-    db.close();
+    return patient
+
 }
 
-async function search(args, context) { 
+function search(args, context) { 
 
     //Our server offer search with these search parameters: family, gender, birthdate, _id, email, name
     const {base_version, _id, _COUNT} = args;
@@ -209,7 +199,7 @@ async function search(args, context) {
         personArray = [...personArray1, ...personArray2, ...personArray3]
 
     }
-    if (identifier) { return mapPatientIdentifierToPatient(identifier)}
+    if (identifier) { return searchByIdentifier(identifier)}
     
     // convert person records into an patient resource object arrays
     let patientArray = personArray.map(person => {return mapPersonToPatientResource(person)});
@@ -241,9 +231,8 @@ async function search(args, context) {
     return bundle;
 }    
 
-function searchById(args, context) {
-    
-    
+async function searchById(args) {
+
     let { base_version, id } = args;
 
     const db = new Database('./persondb.db', { verbose: console.log })
@@ -251,10 +240,15 @@ function searchById(args, context) {
     let personFound= stmt.get(id)
 
     if (personFound) {
-        return mapPersonToPatientResource(personFound)
+
+        R = await mapPersonToPatientResource(personFound)
+        R = await mapPersonDocRecordToPatientIdentifier(personFound)
+
+        return R;
+2
     } else {
         let OO = new OperationOutcome();
-        var message = "Patient with identifier "+ id + " not found ";
+        var message = "Patient with id "+ id + " not found ";
         OO.issue = [{
             "severity": "error",
             "code": "processing",
@@ -266,47 +260,55 @@ function searchById(args, context) {
 
 //The incoming request has context, which has one "req" object, which has request body,  which contains resourse. //Note: Only JSON is supported
 // we extract values from request body and set it to different fields in the data table
-function create(args, context) {
+async function create(args, context) {
 
     // Extract person values from context.req.body
     resource = context.req.body;
 
-    lastName = resource.name[0].family;
-    firstName = resource.name[0].given[0];
-    secondName = resource.name[0].given[1];
-    birthDate = resource.birthDate;
-    gender = resource.gender;
-    email = resource.telecom[0].value;
-    nickname = resource.name[1].given[0];
-    createdAt = new Date().toISOString()
-    updatedAt = " "
+    let lastName = resource.name[0].family;
+    let firstName = resource.name[0].given[0];
+    // use tertiary operator to provide value conditionally
+    let secondName =  (resource.name[0].given[1]) ? resource.name[0].given[1] : "N/A"
+    let birthDate = resource.birthDate;
+    let gender = resource.gender;
+    let email = resource.telecom[0].value;
+    let nickname = resource.name[1].given[0];
+    let createdAt = new Date().toISOString()
+    // let updatedAt = " "
 
     // Match value to keys(columnNames)
-    let columnNames = '"PRSN_FIRST_NAME","PRSN_SECOND_NAME","PRSN_LAST_NAME","PRSN_BIRTH_DATE", "PRSN_GENDER","PRSN_EMAIL","PRSN_NICK_NAME","PRSN_CREATE_DATE"'
+    let columnNames = "(PRSN_FIRST_NAME,PRSN_SECOND_NAME,PRSN_LAST_NAME,PRSN_BIRTH_DATE,PRSN_GENDER,PRSN_EMAIL,PRSN_NICK_NAME,PRSN_CREATE_DATE)"
 
-    let personValues = `firstName,secondName,lastName,birthDate,gender,email,nickname,createdAt`
+    // let personValues = firstName, secondName,lastName, birthDate, gender, email, nickname, createdAt
 
-      const db = new Database('./persondb.db', { verbose: console.log })
+    const db = new Database('./persondb.db', { verbose: console.log })
 
-      const stmt = db.prepare(`INSERT INTO PERSON (${columnNames}) VALUES (?, ?, ?, ?, ?, ?, ?,?)`);
-      const info = stmt.run(personValues);
+    const stmt = db.prepare(`INSERT INTO PERSON ${columnNames} VALUES (?, ?, ?, ?, ?, ?, ?, ?)`);
+    const info = stmt.run(firstName, secondName,lastName, birthDate, gender, email, nickname, createdAt);
 
-      // info.lastInsertRowid: the rowid of the last row inserted
-      let newId = info.lastInsertRowId
-        //This is the new resource id (server assigned)
-        personId = newId
-        resource.identifier.forEach((identifier) => {
-            return createOnePersonDocRecord(personId,identifier)
-        })
+    //Server assigned a new personId for the new person we created, which is stored in the info object (better-sqlite3 created it)
+    let personId = info.lastInsertRowid
+    // create this new person's related PERSON_DOC record(s) from patient resource's identifier data
+    
+    //The request has the identifier data stored in resource.identifier (an array)
+    // We use the new personId and the identifier info from the req.resource to create new PERSON_DOC record(s)
+    await resource.identifier.forEach((identifier) => {
+        createPersonDocRecord(personId,identifier)
+    })
 
-      db.close();
+    //We create the response object, which has the personId of the newly created person.
+    console.log("create patient, new patient Id", personId)
+    return {
+        id: personId,
+      };
+    // db.close();
 }
 
 module.exports = {
     mapPersonToPatientResource,
-    mapPersonDocToPatientIdentifier,
-    mapPatientIdentifierToPatient,
-    createOnePersonDocRecord,
+    mapPersonDocRecordToPatientIdentifier,
+    createPersonDocRecord,
+    searchByIdentifier,
     search, 
     searchById,
     create
